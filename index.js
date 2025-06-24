426b513d13e6eac0589038eb370b3aa65cb14756
Breno Reis <reisb6774@gmail.com>
2025-06-24 19:48:25 -0300

    feat: Adding comments to the code
    Adding some comments to the code and changing some lines to make  it more readable and clean.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-let count = 0
+
+// Getting the elements by their ID
+
 const decreasebutton = document.getElementById('decreasebutton')
 const resetbutton = document.getElementById('resetbutton')
 const increasebutton = document.getElementById('increasebutton')
 const counterlabel = document.getElementById('counterlabel')
 
+
+
+// Adding a counter 
+
+let count = 0 
+
+// Making the backend logic
 
 decreasebutton.onclick = function(){
     count--;
@@ -19,6 +28,8 @@ increasebutton.onclick = function(){
     count ++;
     Updatedisplay()
 }
+
+// Displaying a new function 
 
 function Updatedisplay (){
     counterlabel.textContent = count
